dad320f41648da1295243727f1def666823352e5
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-26 08:25:34 +0100

    feat: parse file to tree root

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ import xml.etree.ElementTree as ET
 # Classes
 
 # Functions
+def file_to_tree(filename):
+    return ET.parse(filename)
+
+def root_from_tree(tree):
+    return tree.getroot()
+
+
+def parse_file(filename):
+    root = root_from_tree(file_to_tree(filename))
 
 # Main
 
 if __name__ == "__main__":
-	pass
+    parse_file("activity.xml")
