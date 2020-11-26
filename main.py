a0a362c20e10122a8d1b62e2e9e0eccab8f5b779
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-26 08:30:38 +0100

    feat: get all track points from tracks

--- main.py
+++ main.py
@@ -20,12 +20,19 @@ def handle_laps(laps):
         tracks += lap.findall('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Track')
     return tracks
 
+def handle_tracks(tracks):
+    track_points = []
+    for track in tracks:
+        track_points += track.findall('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Trackpoint')
+    return track_points
+
 
 def parse_file(filename):
     root = root_from_tree(file_to_tree(filename))
     activity = root[0][0]
     laps = activity.findall("{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Lap")
     tracks = handle_laps(laps)
+    track_points = handle_tracks(tracks)
 
 # Main
 
