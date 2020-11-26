685f1c740bf56b65f1f7275b60cfb652390e8689
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-26 08:28:23 +0100

    feat: find all laps and handle them to get all tracks

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ def file_to_tree(filename):
 def root_from_tree(tree):
     return tree.getroot()
 
+def handle_laps(laps):
+    tracks = []
+    for lap in laps:
+        tracks += lap.findall('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Track')
+    return tracks
+
 
 def parse_file(filename):
     root = root_from_tree(file_to_tree(filename))
     activity = root[0][0]
+    laps = activity.findall("{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Lap")
+    tracks = handle_laps(laps)
 
 # Main
 
