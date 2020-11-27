65443dc12df10cc0947cdd4a837d69ffe9cd2781
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-27 09:15:56 +0100

    feat: Make it possible to load files

--- main.py
+++ main.py
@@ -3,16 +3,32 @@
 # Imports
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 # Variable declarations
 root = tk.Tk()
 
 # Classes
 class LoadFilesPage(ttk.Frame):
+    plot = Figure()
 
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.parent = parent
 
+        load_button = ttk.Button(self, text="Load File",command=self.load_file)
+        load_button.pack()
+        files_label = ttk.Label(self, text="Files loaded:")
+        files_label.pack()
+
+
+    def load_file(self):
+       filename = filedialog.askopenfilename(
+               initialdir="/", 
+               title="Select file to load", 
+               filetypes=(("xml files", ".xml"), ("training files", "*.tcx"), ("all files", "*.*"))) 
+       label = ttk.Label(self, text=filename)
+       label.pack()
+    
 
 # Functions
 
